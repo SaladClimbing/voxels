b842b8968959c850519262eea85473ae4414cb7a
SaladClimbing <84188441+SaladClimbing@users.noreply.github.com>
2024-03-29 13:17:24 -0400

    Definetly didn't steal 3D camera code (https://bevy-cheatbook.github.io/3d/camera.html)

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,26 @@
 
 use bevy::prelude::*;
 
+// I don't know what this does entirely
+// Rust syntax is still new
+// https://bevy-cheatbook.github.io/3d/camera.html
+#[derive(Component)]
+struct MyCameraMarker;
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(10.0, 12.0, 16.0)
+                .looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        MyCameraMarker,
+    ));
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup_camera)
         .run();
 }
